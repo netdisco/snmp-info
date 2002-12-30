eff433529858ef25dc62006b4d13dbcdcd1845cf
Max Baker <>
2002-12-30 21:26:48 +0000

    Initial Import.  Ver 0.1

---
+++ test.pl
@@ -0,0 +1,17 @@
+# SNMP::Info - test.pl
+#
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl test.pl'
+#
+
+use Test;
+BEGIN { plan tests => 1 };
+
+use SNMP::Info;
+ok(1); 
+
+# If we made it this far, we're ok.
+#########################
+
+# No tests yet.
+print "No test's implemented yet. \n";
