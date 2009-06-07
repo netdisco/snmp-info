98eceb7c57e5b01ddc12b28e1eeeecb687a7a8e8
Max Baker <>
2009-06-07 22:50:04 +0000

    add cisco id by OID for vendor for some problem devices. c/o Sam Stickland

--- Info/Layer3.pm
+++ Info/Layer3.pm
@@ -286,7 +286,12 @@ sub vendor {
     my $l3 = shift;
 
     my $descr = $l3->description();
+    my $id = $l3->id();
 
+    # .1.3.6.1.4.1.9.1 is the CISCO-PRODUCTS-MIB
+    # .1.3.6.1.4.1.9.9.368.4 is an old tree that Cisco CSSs were numbered from
+    return 'cisco'   if $id =~ /^\Q.1.3.6.1.4.1.9.1.\E\d+$/;
+    return 'cisco'   if $id =~ /^\Q.1.3.6.1.4.1.9.9.368.4.\E\d+/;
     return 'cisco'   if ( $descr =~ /(cisco|\bios\b)/i );
     return 'foundry' if ( $descr =~ /foundry/i );
 
