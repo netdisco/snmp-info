3eae0b9d5c0920752e82a6f3199c0f59c38c7e10
Oliver Gorwits <oliver@cpan.org>
2013-05-21 09:35:38 +0100

    Support Alcatel devices with layer3 features.

--- Info.pm
+++ Info.pm
@@ -1402,6 +1402,7 @@ sub device_type {
         3375  => 'SNMP::Info::Layer3::F5',
         4526  => 'SNMP::Info::Layer2::Netgear',
         5624  => 'SNMP::Info::Layer3::Enterasys',
+        6486  => 'SNMP::Info::Layer3::AlcatelLucent',
         11898 => 'SNMP::Info::Layer2::Orinoco',
         14179 => 'SNMP::Info::Layer2::Airespace',
         14525 => 'SNMP::Info::Layer2::Trapeze',
