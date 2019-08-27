ebf28f7091258947be3d8a28711c8665c496e52f
Ryan Gasik <pyro3d@gmail.com>
2019-08-26 21:49:42 -0700

    Begin Ciena Implementation

---
+++ lib/SNMP/Info/Layer3/Ciena.pm
@@ -0,0 +1,102 @@
+# SNMP::Info::Layer3::Ciena - SNMP Interface to Ciena Devices
+#
+# Copyright (c) 2019 by The Netdisco Developer Team.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the University of California, Santa Cruz nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR # ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+package SNMP::Info::Layer3::Ciena;
+
+use strict;
+use warnings;
+use Exporter;
+use SNMP::Info::Layer3;
+
+@SNMP::Info::Layer3::Ciena::ISA       = qw/SNMP::Info::Layer3 Exporter/;
+@SNMP::Info::Layer3::Ciena::EXPORT_OK = qw//;
+
+our ($VERSION, %GLOBALS, %MIBS, %FUNCS, %MUNGE);
+
+$VERSION = '3.68';
+
+%MIBS = (
+    %SNMP::Info::Layer3::MIBS,
+    'WWP-LEOS-SW-XGRADE-MIB'    => 'wwpLeosBladeRunPackageVer',
+    'WWP-LEOS-BLADE-MIB'        => 'wwpLeosBladeId',
+    'WWP-LEOS-CHASSIS-MIB'      => 'wwpLeosChassisDeviceId',
+    'WWP-LEOS-FLOW-MIB'         => 'wwpLeosFlowLearnType',
+    'WWP-LEOS-PORT-MIB'         => 'wwpLeosEtherIngressPvid',
+    'WWP-LEOS-VLAN-TAG-MIB'     => 'wwpLeosNumVlans'
+);
+
+%GLOBALS = (
+    'serial'    => 'wwpLeosSystemSerialNumber',
+    'mac'       => 'wwpLeosChassisMacAddress',
+    'box_descr' => 'wwpLeosPhyBladeBoardDesc',
+    'version'   => 'wwpLeosBladeRunPackageVer'
+);
+
+%FUNCS = (
+    %SNMP::Info::Layer3::FUNCS,
+    'v_name'    => 'wwpLeosVlanName'
+);
+
+%MUNGE = (
+    %SNMP::Info::Layer3::MUNGE
+);
+
+sub vendor {
+    return 'ciena';
+}
+
+sub os {
+    return 'SAOS';
+}
+
+
+sub i_vlan {
+    my $ciena = shift;
+    my $i_vlan = {};
+    my $pvid = $ciena->wwpLeosEtherIngressPvid() || undef;
+
+    if (defined $pvid) {
+        foreach my $i (keys %$pvid) {
+            $i_vlan->{$i+10000} = $pvid->{$i};
+        }
+    }
+    return $i_vlan;
+}
+
+sub i_vlan_membership {
+    my $ciena = shift;
+    my $i_vlan_membership = {};
+ 
+    my $vlans = $ciena->wwpLeosVlanMemberPortId();
+
+    foreach my $vlan (keys %$vlans) {
+        push @{$i_vlan_membership->{$vlans->{$vlan}+10000}} , (split(/\./,$vlan))[0];
+    }
+    return $i_vlan_membership;
+    }
