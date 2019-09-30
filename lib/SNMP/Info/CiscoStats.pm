922f137d48251a4bb113e571849b6e47926a0618
Christoph Neuhaus <chr.neuhaus@paecker.net>
2019-09-30 15:33:23 +0200

    Gibraltar Codename added

--- lib/SNMP/Info/CiscoStats.pm
+++ lib/SNMP/Info/CiscoStats.pm
@@ -108,6 +108,7 @@ sub os {
     return 'ios-xe'   if ( $descr =~ /Denali/ );
     return 'ios-xe'   if ( $descr =~ /Fuji/ );
     return 'ios-xe'   if ( $descr =~ /Everest/ );
+    return 'ios-xe'   if ( $descr =~ /Gibraltar/ );
     return 'ios-xe'   if ( $descr =~ /IOS-XE/ );
     return 'ios-xr'   if ( $descr =~ /IOS XR/ );
     return 'ios'      if ( $descr =~ /IOS/ );
