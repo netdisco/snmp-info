c09c1ea21bf21f1e113803479307202e1df0a4a9
Jeroen van Ingen <jeroen@utwente.nl>
2020-03-13 10:58:25 +0100

    Add initial support for Teltonika RUT9xx routers

--- lib/SNMP/Info.pm
+++ lib/SNMP/Info.pm
@@ -1038,6 +1038,12 @@ Subclass for Avaya Secure Routers.
 
 See documentation in L<SNMP::Info::Layer3::Tasman> for details.
 
+=item SNMP::Info::Layer3::Teltonika
+
+Subclass for Teltonika RUT9xx series routers.
+
+See documentation in L<SNMP::Info::Layer3::Teltonika> for details.
+
 =item SNMP::Info::Layer3::Timetra
 
 Alcatel-Lucent SR Class.
@@ -1923,6 +1929,10 @@ sub device_type {
         $objtype = 'SNMP::Info::Layer3::Scalance'
 	    if ( $soid =~ /\.1\.3\.6\.1\.4\.1\.4329\.6\.1\.2/i );
 
+        # Teltonika RUT9xx Series
+        $objtype = 'SNMP::Info::Layer3::Teltonika'
+            if (
+            $desc =~ /\bTeltonika.*RUT9\d{2}\b/);
 
         # Generic device classification based upon sysObjectID
         if (    ( $objtype eq 'SNMP::Info::Layer3' )
@@ -2133,6 +2143,11 @@ sub device_type {
         # it would flip/flop between those
         $objtype = 'SNMP::Info::Layer3::Scalance'
 	    if ( $soid =~ /\.1\.3\.6\.1\.4\.1\.4329\.6\.1\.2/i );
+
+        # Teltonika RUT9xx Series
+        $objtype = 'SNMP::Info::Layer3::Teltonika'
+            if (
+            $desc =~ /\bTeltonika.*RUT9\d{2}\b/);
 
         # Generic device classification based upon sysObjectID
         if ( defined($id) and $objtype eq 'SNMP::Info') {
